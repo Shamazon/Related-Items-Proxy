4cbc26fb743df29adcda36ef6ddc24ab99540124
Zachary Hill <zach@zacharyhill.xyz>
2018-04-18 14:11:14 -0700

    Re-implement in React Router v3

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Router, Route, Link, IndexRoute, browserHistory } from "react-router-3";
 
 import RelatedProducts from './components/App.jsx';
 
 const App = (props) => {
-  const id = props.match.params.id || 0;
+  const id = Number(props.location.pathname.substr(1));
   return (
     <RelatedProducts id={id} />
   );
 };
 
 ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Route exact path="/" component={App} />
+  <Router history={browserHistory}>
+    <Route path="/">
+      <IndexRoute component={App} />
       <Route path="/:id" component={App} />
-    </div>
-  </BrowserRouter>
+    </Route>
+  </Router>
   , document.getElementById('relatedProducts'));
