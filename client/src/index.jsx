7ee3a450209fdd6d08a15b5d683d1d6ef335a23c
Zachary Hill <zach@zacharyhill.xyz>
2018-04-17 18:11:47 -0700

    Setup basic proxy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import React Router
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import RelatedProducts from './components/App.jsx';
 
-ReactDOM.render(React.createElement(RelatedProducts), document.getElementById('relatedProducts'));
+const App = (props) => {
+  const id = props.match.params.id || 0;
+  return (
+    <RelatedProducts id={id} />
+  );
+};
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/:id" component={App} />
+    </div>
+  </BrowserRouter>
+  , document.getElementById('relatedProducts'));
